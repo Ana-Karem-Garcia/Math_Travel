a055590a119a68f8a0c779987d3a37d0a2003518
ivan-math <142260687+ivan-math@users.noreply.github.com>
2023-12-06 16:05:43 -0600

    Update costos.py

--- MathTravel/costos.py
+++ MathTravel/costos.py
@@ -3,5 +3,5 @@ Función para calcular el costo entre dos ciudades de la matriz de costos
 
 """
 
-def calcular_costo(ciudad1, ciudad2):
+def calcular_costo(matriz_costo,ciudad1, ciudad2):
     return matriz_costo[ciudad1][ciudad2]
