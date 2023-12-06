e0691ce3670cec9087940d866532208484018e7d
Ana-Karem-Garcia <142260985+Ana-Karem-Garcia@users.noreply.github.com>
2023-12-06 15:26:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='Math_Travel',
+setup(name='MathTravel',
       version='0.1',
       description='A Python package for something',
       url='https://github.com/Ana-Karem-Garcia/Math_Travel',
       author='Karem Garcia, Ivan Delgado, Claudia Salas, Tamara Popoca', 
       author_email='ga382900@uaeh.edu.mx , de375363@uaeh.edu.mx , sa450250@uaeh.edu.mx , po357607@uaeh.edu.mx',
       license='MIT',
-      packages=['Math_Travel'],
+      packages=['MathTravel'],
       zip_safe=False)
