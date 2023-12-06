fdce59e276457c5bfa31f78166e4541b51f36cad
Ana-Karem-Garcia <142260985+Ana-Karem-Garcia@users.noreply.github.com>
2023-12-06 15:03:11 -0600

    Función costos
    Función que calcula los costos entre ciudades

---
+++ Math_Travel/costos_entre_ciudades.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Función para calcular el costo entre dos ciudades de la matriz de costos
+
+"""
+
+def calcular_costo(ciudad1, ciudad2):
+    return matriz_costo[ciudad1][ciudad2]
