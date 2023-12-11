3edab721e7d83a89675dfc0eb0340465043f039e
Ana-Karem-Garcia <142260985+Ana-Karem-Garcia@users.noreply.github.com>
2023-12-10 19:11:43 -0600

    Update costos.py

--- MathTravel/costos.py
+++ MathTravel/costos.py
@@ -2,7 +2,6 @@
 Función para calcular el costo entre dos ciudades de la matriz de costos
 
 """
-matriz_costo = []
 
 def calcular_costo(ciudad1, ciudad2):
     return matriz_costo[ciudad1][ciudad2]
