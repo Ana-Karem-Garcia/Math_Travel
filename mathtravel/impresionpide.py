5ad6fb996af98af59d2a91e679fd6d27ab678c1b
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-12-13 10:58:56 -0600

    Update impresionpide.py

--- mathtravel/impresionpide.py
+++ mathtravel/impresionpide.py
@@ -2,6 +2,6 @@
 Funcion que imprime los resultados
 """
 
-def imprime(costo_optimo,ruta_optima):
+def imprime_pide(costo_optimo,ruta_optima):
     print("Costo óptimo:", costo_optimo)
     print("Ruta óptima:", ruta_optima)
