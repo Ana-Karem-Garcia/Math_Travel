676430bf616161dbb14c6f3467e3858fdc4b2cdd
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-12-13 10:49:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup
 
-setup(name='MathTravel',
+setup(name='mathtravel',
       version='0.1',
       description='A Python package to solve the Traveling Salesman problem',
       url='https://github.com/Ana-Karem-Garcia/Math_Travel',
       author='Karem Garcia, Ivan Delgado, Claudia Salas, Tamara Popoca', 
       author_email='ga382900@uaeh.edu.mx , de375363@uaeh.edu.mx , sa450250@uaeh.edu.mx , po357607@uaeh.edu.mx',
       license='MIT',
-      packages=['MathTravel'],
+      packages=['mathtravel'],
       zip_safe=False)
