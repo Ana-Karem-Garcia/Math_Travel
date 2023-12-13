2c986bcda7183001c8763d4d7f7db2257f0da6dd
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-12-13 11:19:35 -0600

    Update impresion.py

--- mathtravel/impresion.py
+++ mathtravel/impresion.py
@@ -1,6 +1,7 @@
 """Funcion que imprime los resultados"""
 
 def imprime(matriz_costo,costo_optimo,ruta_optima):
+    """Imprime los resultados"""
     print("La matriz de costos entre ciudades es:")
     print(matriz_costo)
     print("Costo óptimo:", costo_optimo)
