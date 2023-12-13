b456c99b847161d30fc4d0e2f46a9c2f9280c306
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-12-13 12:25:46 -0600

    Update matrizaleatoria.py

--- mathtravel/matrizaleatoria.py
+++ mathtravel/matrizaleatoria.py
@@ -1,10 +1,8 @@
 """Funcion que hace una matriz aleatoria"""
 
-import numpy as np
-
 def generar_matriz(ciudades, rango_inicio=10, rango_fin=100):
     """Crea una matriz aleatoria de costos"""
-    matriz = np.random.randint(rango_inicio, rango_fin, size=(ciudades, ciudades), dtype=int)
+    matriz = numpy.np.random.randint(rango_inicio, rango_fin, size=(ciudades, ciudades), dtype=int)
     matriz_simetrica = (matriz + matriz.T) // 2
     np.fill_diagonal(matriz_simetrica, 0)
     return matriz_simetrica
