14c8aa306e2b83659c9b896b22b88ede0cc77a01
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-12-13 12:26:39 -0600

    Update matrizaleatoria.py

--- mathtravel/matrizaleatoria.py
+++ mathtravel/matrizaleatoria.py
@@ -4,5 +4,5 @@ def generar_matriz(ciudades, rango_inicio=10, rango_fin=100):
     """Crea una matriz aleatoria de costos"""
     matriz = numpy.np.random.randint(rango_inicio, rango_fin, size=(ciudades, ciudades), dtype=int)
     matriz_simetrica = (matriz + matriz.T) // 2
-    np.fill_diagonal(matriz_simetrica, 0)
+    numpy.np.fill_diagonal(matriz_simetrica, 0)
     return matriz_simetrica
